79408b6c4595e309b1154ade513ff61a01d28770
Breck Cowan <breckcowan@gmail.com>
2021-07-15 21:27:16 -0500

    added function of adding items to the html in <ul>

--- app.js
+++ app.js
@@ -1,78 +1,48 @@
 //jshint esversion:6
-//all logic and code stored in app.js
-//making express and bodyParser required
+
 
 const express = require("express");
 const bodyParser = require("body-parser");
 
-//declaring app constant
-
 const app = express();
 
-//adding in ejs
+var items = ["Buy Food", "Eat Food", "Cook Food"];
 
-app.set('view engine', 'ejs');
+app.set('view engine', "ejs");
 
-//sets app function
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get("/", function(req, res) {
-
-  //sets variables for days of the weekday
-
-  //sets today to get the current day
+app.get("/", function (req, res) {
 
   var today = new Date();
 
-  //sets current day to the previous variables value
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-  var currentDay = today.getDay();
+  var day = today.toLocaleDateString("en-US", options);
 
-  //adding variable to correspond to the ejs marker on index.html file
 
-  var day = "";
-
-  //adding in switch statement to print to client day of the weekday
-  //setting the expression to 'currentDay' variable = number day
-  //setting case variable to 'day' variables value = '"kindOfDay"'
-  //creating new cases that correspond to the 'day' of the week (0,1,2,3,4,5,6,7)
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-//the console log here is meant to help debug if switch statement ever breaks
-    default:
-    console.log("Error: current day is equal to: " + currentDay);
-  }
-//render file called list and pass file (list.ejs) variable kindOfDay and the value is day
-
-  res.render('list', {
-    kindOfDay: day
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items
   });
 
 });
 
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
 
+  items.push(item);
 
-//tells which port server is running on
+  res.redirect("/");
 
-app.listen(3000, function() {
-  console.log("Server is running on port 3000");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
 });
