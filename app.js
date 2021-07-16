7a1853568f3ed880eb39a10501ed47ed11684120
Breck Cowan <breckcowan@gmail.com>
2021-07-16 17:51:24 -0500

    added about page.  using ejs to style other files similarly.

--- app.js
+++ app.js
@@ -53,11 +53,9 @@ app.get("/work", function(req, res) {
   });
 });
 
-app.post("/work", function(req, res) {
-  let item = req.body.newItem;
-  workItems.push(item);
-  res.redirect("/work");
-});
+app.get("/about", function(req,res) {
+  res.render("about");
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
