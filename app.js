ace89ef7ee6a2a14644f6a42a484ce06942c0d4f
Breck Cowan <breckcowan@gmail.com>
2021-07-16 11:10:11 -0500

    added function to be able to add todo list items to the work todo list page

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ let workItems = [];
 
 app.set('view engine', "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
 
   let today = new Date();
 
@@ -26,19 +28,29 @@ app.get("/", function (req, res) {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {  listTitle: day,  newListItems: items  });
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
 
 });
 
-app.post("/", function (req, res) {
+app.post("/", function(req, res) {
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
-
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
 app.get("/work", function(req, res) {
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
 });
 
 app.post("/work", function(req, res) {
@@ -47,6 +59,6 @@ app.post("/work", function(req, res) {
   res.redirect("/work");
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
